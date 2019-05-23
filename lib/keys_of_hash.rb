6e30dad63bc2fcb416e72a67370bf3e52d6dabbc
Jason Leach <jlspursfan@gmail.com>
2019-05-23 00:08:58 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,8 +1,8 @@
 class Hash
   def keys_of(*arguments)
     arr = []
-      if Hash.values.include?(arguments)
-        arr << Hash.key
+      if hash.values.include?(arguments)
+        arr << hash.key
       end
     return arr
   end
